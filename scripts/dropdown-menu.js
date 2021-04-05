2bd02e9a898cf8e97a2c224a601948c1dd06efc1
Michail <denisenko.miiha@mail.ru>
2021-04-06 00:34:36 +0700

    complete dropdown-menu.js

--- scripts/dropdown-menu.js
+++ scripts/dropdown-menu.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
    document.querySelectorAll('.style-menu__btn').forEach(function(el) {
        el.addEventListener('click', function(event) {
-        document.querySelector('.submenu').classList.toggle('submenu_is-active')
+        el.nextElementSibling.classList.toggle('submenu_is-active')
        })
    })
 })
