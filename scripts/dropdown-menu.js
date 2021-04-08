a29377b2fb9486a778c0b1d75626089438e23326
Michail <denisenko.miiha@mail.ru>
2021-04-08 17:10:48 +0700

    fix dropdown js code and dropdown elements

--- scripts/dropdown-menu.js
+++ scripts/dropdown-menu.js
@@ -1,7 +1,19 @@
 window.addEventListener('DOMContentLoaded', function() {
    document.querySelectorAll('.style-menu__btn').forEach(function(el) {
        el.addEventListener('click', function(event) {
-        el.nextElementSibling.classList.toggle('submenu_is-active')
+           console.log(el.nextElementSibling.classList.contains('submenu_is-active'))
+           if (el.nextElementSibling.classList.contains('submenu_is-active')) {
+               el.nextElementSibling.classList.remove('submenu_is-active')
+           } else {
+                document.querySelectorAll('.submenu').forEach(function(el) {
+                    el.classList.remove('submenu_is-active')
+                })
+               el.nextElementSibling.classList.add('submenu_is-active')
+           }
+        //    document.querySelectorAll('.submenu').forEach(function(el) {
+        //        el.classList.remove('submenu_is-active')
+        //    })
+        //    el.nextElementSibling.classList.toggle('submenu_is-active')
        })
    })
 })
