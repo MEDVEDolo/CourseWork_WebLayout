c63d712fc696984b5dda778dfebf5915e2c38e7e
Michail <denisenko.miiha@mail.ru>
2021-04-12 21:07:01 +0700

    add effects link and btn

--- scripts/dropdown-menu.js
+++ scripts/dropdown-menu.js
@@ -4,16 +4,22 @@ window.addEventListener('DOMContentLoaded', function() {
            console.log(el.nextElementSibling.classList.contains('submenu_is-active'))
            if (el.nextElementSibling.classList.contains('submenu_is-active')) {
                el.nextElementSibling.classList.remove('submenu_is-active')
+               el.classList.remove('style-menu__btn_active')
+               el.classList.add('style-menu__btn_no-active')
            } else {
                 document.querySelectorAll('.submenu').forEach(function(el) {
                     el.classList.remove('submenu_is-active')
                 })
+
+                document.querySelectorAll('.style-menu__btn').forEach(function(el) {
+                    el.classList.remove('style-menu__btn_active')
+                    el.classList.add('style-menu__btn_no-active')
+                })
+
                el.nextElementSibling.classList.add('submenu_is-active')
+               el.classList.add('style-menu__btn_active')
+               el.classList.remove('style-menu__btn_no-active')
            }
-        //    document.querySelectorAll('.submenu').forEach(function(el) {
-        //        el.classList.remove('submenu_is-active')
-        //    })
-        //    el.nextElementSibling.classList.toggle('submenu_is-active')
        })
    })
 })
