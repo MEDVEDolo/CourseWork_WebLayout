da8d6979c1642804891ebdb5c4ef6cfbab94ca48
Michail <denisenko.miiha@mail.ru>
2021-09-02 18:56:09 +0700

    fix select

--- scripts/select.js
+++ scripts/select.js
@@ -2,6 +2,7 @@ const defaultSelect = () => {
     const element = document.querySelector('.custom-select');
     const choices = new Choices(element, {
         searchEnabled: false,
+        renderSelectedChoices: 'always',
     });
 }
 
